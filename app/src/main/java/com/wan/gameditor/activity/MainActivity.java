6c5d8381a0fb03d1d265b283e6a9895ca96d895a
Stars-One <Stars-One@users.noreply.github.com>
2018-02-01 13:10:28 +0800

    增加从图库选择图片替换头部图片功能

--- app/src/main/java/com/wan/gameditor/activity/MainActivity.java
+++ app/src/main/java/com/wan/gameditor/activity/MainActivity.java
@@ -1,11 +1,24 @@
 package com.wan.gameditor.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.content.ContentUris;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,11 +28,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.wan.gameditor.R;
+
+import static com.wan.gameditor.activity.SettingActivity.CHOOSE_PHOTO;
 
 
 /**
@@ -32,7 +48,8 @@ public class MainActivity extends AppCompatActivity
 
     private long exitTime = 0;
     private ImageView mImageHead;
-
+    private ImageButton mImageButton;
+    private ImageView headImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,9 +94,15 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent intent2 = new Intent(this,SettingActivity.class);
+                startActivity(intent2);
+                break;
+            default:break;
+        }
 
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -125,6 +148,9 @@ public class MainActivity extends AppCompatActivity
         mLinearlayout = (LinearLayout) findViewById(R.id.linearlayout);
         mImageHead = (ImageView) findViewById(R.id.image_head);
         mImageHead.setOnClickListener(this);
+        mImageButton = (ImageButton) findViewById(R.id.changePicture);
+        mImageButton.setOnClickListener(this);
+        headImage = (ImageView) findViewById(R.id.headPicture);
     }
 
 
@@ -184,13 +210,117 @@ public class MainActivity extends AppCompatActivity
                 break;
             default:
                 break;
+            case R.id.changePicture:
+                checkPermission();
+                break;
             case R.id.image_head:
                 enterchoseActivity("头饰");
                 break;
         }
     }
-public void enterchoseActivity(String s){
+    //检查权限
+    public void checkPermission(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            //发现没有权限，调用requestPermissions方法向用户申请权限，requestPermissions接收三个参数，第一个是context，第二个是一个String数组，我们把要申请的权限
+            //名放在数组中即可，第三个是请求码，只要是唯一值就行
+        }else{
+            openAlbum();//有权限就打开相册
+        }
+    }
 
-}
+    public void openAlbum(){
+        //通过intent打开相册，使用startactivityForResult方法启动actvity，会返回到onActivityResult方法，所以我们还得复写onActivityResult方法
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+    //弹出窗口向用户申请权限
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);//弹出授权的窗口，这条语句也可以删除，没有影响
+        //获得了用户的授权结果，保存在grantResults中，判断grantResult中的结果来决定接下来的操作
+        switch (requestCode){
+            case 1:
+
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this, "授权失败，无法操作", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case CHOOSE_PHOTO:
+                if (resultCode==RESULT_OK){
+                    if (Build.VERSION.SDK_INT>=19){
+                        handleImageOnkitKat(data);//高于4.4版本使用此方法处理图片
+                    }else{
+                        handleImageBeforeKitKat(data);//低于4.4版本使用此方法处理图片
+                    }
+                }
+                break;
+            default:break;
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnkitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            } else if ("com.android,providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+
+        }else if("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri,null);
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitKat(Intent data){
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+    //获得图片路径
+    public String getImagePath(Uri uri,String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);   //内容提供器
+        if (cursor!=null){
+            if (cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));   //获取路径
+            }
+        }
+        cursor.close();
+        return path;
+    }
+    //展示图片
+    private void displayImage(String imagePath) {
+        if (imagePath != null){
+            Bitmap bitImage = BitmapFactory.decodeFile(imagePath);//格式化图片
+            headImage.setImageBitmap(bitImage);//为imageView设置图片
+        }
+        else{
+            Toast.makeText(MainActivity.this,"获取图片失败",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 }
